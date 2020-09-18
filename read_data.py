c9e240de0af5887ff571c7b82cbd96ad15f4ce70
Behrouz Safari <behrouz.safari@gmail.com>
2020-09-18 17:20:01 +0200

    Groupby countries

--- read_data.py
+++ read_data.py
@@ -8,7 +8,7 @@ adr_dth = adr + 'csse_covid_19_time_series/time_series_covid19_deaths_global.csv
 adr_rcv = adr + 'csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
 
 
-def read_df(url):
+def read_regions(url):
     '''Read time series df and return daily df'''
     df = pd.read_csv(url).drop(['Lat', 'Long'], axis=1)
     df['Province/State'] = df['Province/State'].fillna('')
@@ -22,7 +22,14 @@ def read_df(url):
     daily = df.diff()
     return daily
 
-# Read data
-cnf = read_df(adr_cnf)
-dth = read_df(adr_dth)
-rcv = read_df(adr_rcv)
+def read_countries(url):
+    df = pd.read_csv(adr_dth).drop(['Province/State', 'Lat', 'Long'], axis=1)
+    df = df.groupby('Country/Region').sum().T
+    df.index = pd.to_datetime(df.index)
+    daily = df.diff()
+    return daily
+    
+
+# Reading deaths
+dth = read_countries(adr_dth)
+
